787314a57b0a6082ecc916fefc8951eba3297729
Lina_Herrera <lina_h64@hotmail.com>
2023-05-23 16:00:11 -0500

    starting JWT implementation

--- MyApp/src/controllers/userController.js
+++ MyApp/src/controllers/userController.js
@@ -2,7 +2,6 @@ const { User } = require("../models");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-
 exports.getAllUsers = async (_, res, next) => {
     try {
         console.log("Correct getAllUsers");
@@ -19,25 +18,26 @@ exports.getUserById = async (req, res, next) => {
         console.log("Correct getUserById");
         const Users = await User.findByPk(id);
         res.json(Users);
-    }catch (error) {
+    } catch (error) {
         next(error);
     }
 };
 
 
-    exports.createUser = async (req, res, next) => {
-        try {
+
+exports.createUser = async (req, res, next) => {
+    try {
         console.log("Correct createUser");
         const user = await User.create(req.body);
         res.json(user);
-    }catch (error) {
+    } catch (error) {
         next(error);
     }
 };
 
-    exports.deleteUser = async (req, res, next) => {
-        const { id } = req.params;
-        try {
+exports.deleteUser = async (req, res, next) => {
+    const { id } = req.params;
+    try {
         console.log("Correct deleteUser");
         const user = await User.findByPk(id);
         if (!user) {
@@ -45,14 +45,14 @@ exports.getUserById = async (req, res, next) => {
         }
         await user.destroy();
         res.json({ message: `User ${user.firstName} ${user.lastName} has been deleted` });
-    }catch (error) {
+    } catch (error) {
         next(error);
     }
 };
 
-    exports.updateUser = async (req, res, next) => {
-        const { id } = req.params;
-        try {
+exports.updateUser = async (req, res, next) => {
+    const { id } = req.params;
+    try {
         console.log("Correct updateUser");
         const user = await User.update(req.body, {
             returning: true,
@@ -61,7 +61,7 @@ exports.getUserById = async (req, res, next) => {
             }
         });
         res.json(user);
-    }catch (error) {
+    } catch (error) {
         next(error);
     }
 };
@@ -75,14 +75,14 @@ exports.login = async (req, res) => {
 
         // Verify user
         if (!user) {
-            return res.status(404).json({ error: "Usuario no encontrado" });
+            return res.status(404).json({ error: "User not found" });
         }
 
         // Compare password 
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (!isPasswordValid) {
-            return res.status(401).json({ error: "Credenciales inválidas" });
+            return res.status(401).json({ error: "Invalid credentials" });
         }
 
         // Create JWT token
@@ -100,6 +100,6 @@ exports.login = async (req, res) => {
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Error en el servidor" });
+        res.status(500).json({ error: "Server error" });
     }
 };
